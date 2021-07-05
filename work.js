cc8ae0893cba44427be382039627293eaca2d0f2
Ganwa Chachar <ganwahc@gmail.com>
2021-07-05 19:03:40 +0500

    javascript practice

---
+++ work.js
@@ -0,0 +1,198 @@
+//========= [ Chapter 1]=========
+// alert("Thank you for your input !");
+
+// window.alert("Thank you for your input 2!");
+// var newName ; //declare
+// newName = "1++54467"; // assign
+// var name = 'Jawer"ia';
+// console.log("name 1", name, 'ghanwa');
+// alert(name)
+
+// name = "Mehak";
+// alert(name);
+
+//console.log("nme2", name, newName);
+//=========[END]=========
+// =========[ Chapter 3 Variables for numbers]=========
+// var age = 18.22;
+// Age = 25;
+// alert(age);
+// alert(Age);
+
+// var height = 5.5;
+// var a = 5;
+// var b = 7;
+
+// var sum = a+b;
+// var subtract = a - b;
+// alert(subtract);
+
+// var sum = a+b;
+// var multiply = a * b;
+// alert(multiply);
+
+// var sum = a+b;
+// var division = a / b;
+// alert(division);
+
+// var weight = 150;
+// var newWeight = weight + 25;
+// weight = weight + 28;
+// weight = 23;
+// alert(weight);
+
+// var originalNum = "23";
+// var newNum = 7 + originalNum;
+// alert(newNum);
+//=========[END]=========
+// =========[ Chapter 4 Variable Names Legal and Illegal]=========
+// userName 
+// var name = "Sunbal";
+// var name_hello = "sunbal";
+// var $name = "ghanwa";
+
+// var 1name = "Sunbal";
+// var name hello = "sunbal";
+// var -name = "ghanwa";
+
+// var name [hello = "iffa";
+// var name >hello = "iffa";
+//  var num = 2;
+//  var newNum = 2;
+
+//  alert(num);
+//  alert(newNum);
+//=========[END]=========
+
+//=========[END]=========
+// [ Chapter 6]=========
+// ex 1
+
+// var a = 8;
+
+// [variable 1]=========
+
+// var b = a + 1;
+
+// alert(a);
+// alert(b);
+
+// [variable 2]=========
+// a = a + 1;
+
+// [variable 3]=========
+// a + = 1;
+
+// [variable 4]=========
+// ex 4
+// a++;
+
+// [Chapter 6]=========
+// ex 3
+// var b = 10;
+// a++;
+// alert(a);
+
+// ++a;
+// alert(a);
+
+//[Chapter 6]=========
+//ex 3;
+
+// var c = 19;
+// var d = ++a;
+// alert(a);
+// alert(b);
+
+//[Chapter 6]=========
+//ex 4;
+// var e = 5;
+// var f = ++e + a++;
+// alert(e);
+// alert(f);
+
+//[Chapter 6]=========
+//ex 4;
+// var a = 4;
+// var b = ++a + --a;
+// alert(a);
+// alert(b);
+
+//[Chapter 6]=========
+//ex 5;
+// var age = 3;
+// var newAge = age++;
+// alert(age);
+// alert(newAge);
+
+//[Chapter 6]=========
+//ex 5;
+// var age = 3;
+// var newAge = ++age;
+// alert(age);
+// alert(newAge);
+
+// =========[Chapter 5 Math expressions: Familiar operators ]=========
+// =========[ Chapter 7 Math expressions: Eliminating ambiguity]=========
+// var a = 5 + 2 * 2-1;
+// var a = (5+(6*2))-1;
+// alert(a);
+// var b = (4+5)*2-1;
+// alert(b);
+// var c = 10;
+// var d = 4;
+
+// var e = c - d * c + d;
+// var f  = 5 - 15 + 3;
+// alert(e);s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
